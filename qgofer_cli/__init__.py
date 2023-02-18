fc5ab21604cb452e872567618eca36a0fc1550f5
acquayefrank <acquayefrank@gmail.com>
2023-02-18 14:41:31 +0000

    switching from click to typer

--- qgofer_cli/__init__.py
+++ qgofer_cli/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """acquayefrank"""
 __email__ = 'dev@qgofer.com'
 __version__ = '0.1.0'
+__license__ = 'MIT'
+__app_name__ = 'qgofer-cli'
--- qgofer_cli/cli.py
+++ qgofer_cli/cli.py
@@ -1,15 +1,33 @@
 """Console script for qgofer_cli."""
 
-import click
+from typing import Optional
+
+import typer
+
+from qgofer_cli import __app_name__, __version__
+
+app = typer.Typer()
 
 
-@click.command()
-def main():
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+
+@app.command()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="All your documents are one quick search away.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
     """Main entrypoint."""
-    click.echo("qgofer-cli")
-    click.echo("=" * len("qgofer-cli"))
-    click.echo("All your documents are one quick search away.")
-
-
-if __name__ == "__main__":
-    main()  # pragma: no cover
+    typer.echo("qgofer-cli")
+    typer.echo("=" * len("qgofer-cli"))
+    typer.echo("All your documents are one quick search away.")
+    return None
