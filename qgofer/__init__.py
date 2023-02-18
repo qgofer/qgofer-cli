83f73e499f4d2870b27f9904205b38da0577c56b
acquayefrank <acquayefrank@gmail.com>
2023-02-18 23:09:46 +0000

    fixed issue with commands not being detected

--- qgofer/__init__.py
+++ qgofer/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "acquayefrank"
 __email__ = 'dev@qgofer.com'
-__version__ = '0.1.1-alpha.7'
+__version__ = '0.1.1-alpha.8'
 __license__ = 'MIT'
 __app_name__ = 'qgofer'
 __description__ = 'All your documents are one quick search away.'
