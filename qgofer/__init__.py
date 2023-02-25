b61f3af73131c26ca5741d8f036e912112c3fc8e
acquayefrank <acquayefrank@gmail.com>
2023-02-25 21:39:55 +0000

    changed versioning

--- qgofer/__init__.py
+++ qgofer/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "acquayefrank"
 __email__ = 'dev@qgofer.com'
-__version__ = '0.0.1-dev0'
+__version__ = '0.0.1-dev1'
 __license__ = 'MIT'
 __app_name__ = 'qgofer'
 __description__ = 'All your documents are one quick search away.'
